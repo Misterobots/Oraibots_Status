0d159ea1ee085fbe34301921cd22cd38c1f9e347
Misterobots <pause.not@gmail.com>
2021-04-15 09:48:20 -0400

    Update and rename Sentry_Alert.sh to sentry_alert.sh

--- sentry_alert.sh
+++ sentry_alert.sh
@@ -10,4 +10,4 @@ curl -s \
  --data parse_mode=HTML \
  --data chat_id=${telegram_chat_id} \
  --data text="<b>${Title}</b>%0A${Message}" \
- --request POST https://api.telegram.org/bot${telegram_bot_token}/sendMessage
+ --request POST https://api.telegram.org/bot$1708336602:AAG00r-1R3h53cC6WeHJpZ_a3aNwBQy4-zw/sendMessage
